5fa80e0311b2328ebab3c35c9a4283e0ff521eac
Nahuel Piguillem <108589022+NahuelUTNPigui@users.noreply.github.com>
2023-04-26 18:16:29 -0600

    todos los graficos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,8 @@ import * as  p5 from 'p5';
 export class AppComponent implements OnInit,OnDestroy {
   
   private p5:any;
+  private p52:any;
+  private p53:any;
   private origin = { x: 0, y: 0 };
   //const randomY:any=[]
   //const numPts:any=25
@@ -27,44 +29,92 @@ export class AppComponent implements OnInit,OnDestroy {
     [2014,    124],
     [2015,    380]
   ]
+  data2=[
+    ["10-04-2023",114],
+    ["09-04-2023",105],
+    ["08-04-2023",102],
+    ["07-04-2023",104],
+    ["19-12-2022",114]
+  ]
   title = 'my-p5-app';
   constructor() {
     console.log('digital-constructed');
     window.onresize = this.onWindowResize;
   }
   private onWindowResize = (e:any) => {
-    this.p5.resizeCanvas(this.p5.windowWidth, this.p5.windowHeight);
+    this.p5.resizeCanvas(750, 450);
   }
   ngOnInit() {
-    console.log('digital-init');
+    
     this.createCanvas();
     
   }
 
   ngOnDestroy(): void {
     this.createCanvas();
-    console.log('audio-destroy');
-  }
-  private createCanvas = () => {
-    console.log('creating canvas');
-    this.p5 = new p5(this.drawing)
     
   }
+  private createCanvas = () => {
+    
+    this.p5 = new p5(this.drawing)
+    this.p52 = new p5(this.drawing2)
+    this.p53 = new p5(this.drawing3)
+  }
+  private drawing2 =(p:any)=>{
+    let randomY:any =[]
+    let numPts:any = 100
+    p.setup=()=>{
+      p.createCanvas(750, 450).parent('line-canvas');
+      for(let i =0; i< numPts; i++){
+        randomY.push(p.random(100,300)); 
+      }
+    }
+    p.draw =()=>{
+      p.background(200)
+      this.drawEllipses(p,randomY,numPts)
+      this.drawLines(p,randomY,numPts)
+    }
+  }
+  private drawEllipses(p:any,randomY:any,numPts:any){
+    p.noStroke();
+      // draw ellipses
+    for(let i =0; i < randomY.length; i++){
+      let x = i * (p.width / (numPts-1));
+      let y = randomY[i];
+      p.ellipse(x, y, 7);
+    }
+  }
   
+  private drawLines(p:any,randomY:any,numPts:any){
+    p.stroke(0);
+   // draw lines
+    let px = 0;
+    let py = randomY[0];
+    for(let i =0; i < randomY.length; i++){
+      let x = i * (p.width / (numPts-1));
+      let y = randomY[i];
+      p.line(px, py, x, y);
+      
+      //store the last position
+      px = x;
+      py = y;
+    }
+  }
   private drawing =  (p: any) => {
     let randomY:any=[]
     let numPts:any=25
-    let d =this.data
+    let d:any =this.data2
     p.setup = () => {
-      p.createCanvas(p.windowWidth, p.windowHeight).parent('digital-watch-canvas');
+      p.createCanvas(750, 450).parent('digital-watch-canvas');
       //p.background(0);
       //console.log(this)
       p.cols=d[0].length
       p.rows=d.length
       p.max=0
       d.forEach((r: any[])  => {
-        if(r[0]>p.max){
-          p.max=r[0]
+        
+        if(r[1]>p.max){
+          p.max=r[1]
         }
       });
     };
@@ -74,18 +124,48 @@ export class AppComponent implements OnInit,OnDestroy {
       p.fill(0)
       
       for (var i = 0; i < p.rows; i++) {
+        p.fill(0)
         //place years
-        p.text(d[i][0], i * 30 + 60, 420);
-        
+        p.text(d[i][0], i * 80 + 60, 420);
+        p.fill("blue")
         //draw graph
-        p.rect(i * 30 + 60, 400 - d[i][1], 20,d[i][1])
+        p.rect(i * 80 + 85, 400 - d[i][1], 20,d[i][1])
       }
+      p.fill(0)
        //determine highest value
        let maxValue=p.max;
       for (var k=0;k<maxValue;k=k+50){
         p.text(k,10,420-k);
       }
     };
+  }
+  private drawing3 = (p:any)=>{
+    let angles = [30, 10, 45, 35, 60, 38, 75, 67];
+    p.setup = () => {
+      p.createCanvas(750, 450).parent('pie-canvas');
+      p.noStroke()
+      p.noLoop()
+    }
+    p.draw =() =>{
+      p.background(100)
+      this.pieChart(p,300,angles,angles)
+    }
+  }
+  private pieChart(p:any,diameter:any, data:any,angles:any) {
+    let lastAngle = 0;
+    for (let i = 0; i < data.length; i++) {
+      let gray = p.map(i, 0, data.length, 0, 255);
+      p.fill(gray);
+      p.arc(
+        p.width / 2,
+        p.height / 2,
+        diameter,
+        diameter,
+        lastAngle,
+        lastAngle + p.radians(angles[i])
+      );
+      lastAngle += p.radians(angles[i]);
+    }
   }
   
   
